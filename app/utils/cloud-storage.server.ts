009ba5c55b8071f60b29a81b47ea7df2f0eb532a
herve26 <nzanzuh@gmail.com>
2024-01-06 17:51:18 +0000

    fix hard coded bucket name

--- app/utils/cloud-storage.server.ts
+++ app/utils/cloud-storage.server.ts
@@ -3,7 +3,8 @@ import { Storage } from '@google-cloud/storage';
 const storage = new Storage();
 
 export async function downloadIntoMemory({fileName}: {fileName: string}) {
-    const bucketName = 'fakab-storage-bucket'
+    const bucketName = process.env["GOOGLE_CLOUD_BUCKET"]
+    if(!bucketName) throw new Error("Bucket Name is Required")
     // Downloads the file into a buffer in memory.
     return (await storage.bucket(bucketName).file(fileName).download())[0];
 }
